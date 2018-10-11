de30e33fb77b454dc7715aa56ba72aeb047572ba
Jiri Vlasak <jirivlasak9@gmail.com>
2018-10-11 14:00:56 +0200

    read sensors test

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python3
+
+import time
+
 from modules.helpers import *
 from modules.IO import IO
 
 def Main():
-    pass
+    io = IO()
+    while(True):
+        debug_print(io.directions_free())
+        time.sleep(1)
 
 if __name__ == "__main__":
     Main()
--- modules/IO.py
+++ modules/IO.py
@@ -1,17 +1,21 @@
 
+import time
+
 from ev3dev2.motor import Motor, LargeMotor
 from ev3dev2.sensor.lego import ColorSensor, UltrasonicSensor
+
 
 class IO():
     """
     Provides functions for high-level IO operations (move left, move forward, is left side free to move etc.).
     """
     def __init__(self):
-        self.lm_left = LargeMotor("")
-        self.lm_right = LargeMotor("")
-        self.sensor_motor = Motor("")
-        self.color_sensor = ColorSensor("")
+        self.lm_left = LargeMotor("outB")
+        self.lm_right = LargeMotor("outA")
+        self.sensor_motor = Motor("outC")
+        self.color_sensor = ColorSensor("in1")
         self.color_sensor.mode = ColorSensor.MODE_COL_REFLECT
+
     
     def go_left(self):
         pass
@@ -33,10 +37,21 @@ class IO():
     
     def __turn_around(self):
         pass
-    
-    
+
     def directions_free(self):
         '''
         Returns tuple of bools of sides (left, center, right)
         '''
-        pass
+        values = []
+        self.sensor_motor.on_for_degrees(-20, 90)
+        values.append(self.color_sensor.reflected_light_intensity)
+        time.sleep(0.5)
+        self.sensor_motor.on_for_degrees(20, 90)
+        values.append(self.color_sensor.reflected_light_intensity)
+        time.sleep(0.5)        
+        self.sensor_motor.on_for_degrees(20, 90)
+        values.append(self.color_sensor.reflected_light_intensity)
+        time.sleep(0.5)        
+        self.sensor_motor.on_for_degrees(-20,90)
+        return values
+
